d1859c833f23efdde0a47a0446ef357e626c691e
Petya Buchkova <petya.buchkova@gmail.com>
2019-05-20 13:21:52 +0200

    Added Eureka Client

--- index.js
+++ index.js
@@ -2,6 +2,39 @@ const application = require('./dist');
 
 module.exports = application;
 
+// ------------------ Eureka Config --------------------------------------------
+
+const Eureka = require('eureka-js-client').Eureka;
+
+const eureka = new Eureka({
+  instance: {
+    app: 'reviewsService',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    statusPageUrl: 'http://localhost:3000',
+    port: {
+      '$': 3000,
+      '@enabled': 'true',
+    },
+    vipAddress: 'localhost',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    }
+  },
+  eureka: {
+    host: 'localhost',
+    port: 8761,
+    servicePath: '/eureka/apps/'
+  }
+});
+eureka.logger.level('debug');
+eureka.start(function (error) {
+  console.log(error || 'complete');
+});
+
+// ------------------ Server Config --------------------------------------------
+
 if (require.main === module) {
   // Run the application
   const config = {
